360743d5f0f038f0f1672d7342658796fa40d9b1
Max Boguslavskiy <max.bogus@gmail.com>
2018-01-06 00:29:15 +0300

    added some art to project. updated view of stop and record buttons. disabled stop button by default.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        stopRecordingButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button pressed")
+        recordingLabel.text = "Recording in progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop button pressed")
+    }
 }
 
